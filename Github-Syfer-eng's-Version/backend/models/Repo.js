1bee6425c2d615df4ef3ce5e664554d5e0c73d96
Website Tree <yes823901@gmail.com>
2025-02-28 17:25:53 -0600

    Delete Github-Syfer-eng's-Version/backend/models directory

--- Github-Syfer-eng's-Version/backend/models/Repo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const RepoSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-});
-
-module.exports = mongoose.model('Repo', RepoSchema);
